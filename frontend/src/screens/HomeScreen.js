bc4508d2a13b503aa3e668ba33286245bdc9b3c8
garden627 <garden627@yahoo.co.jp>
2021-01-17 16:34:25 +0900

    React Hooks & Redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+// import axios from 'axios'
 import Product from '../components/Product'
-import data from '../data'
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
+  // const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  const productList = useSelector(state => state.productList);
+  const dispatch = useDispatch()
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts())
+  //   const fetchData = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const { data } = await axios.get('/api/products');
+  //       setLoading(false);
+  //       setProducts(data);
+  //     } catch(error) {
+  //       setError(error.message);
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchData();
+  }, [dispatch])
   return (
     <div>
-      <div className="row center">
-        {data.products.map((product)=>(
+      {loading? (<LoadingBox></LoadingBox>) : error? (<MessageBox variant="danger">{error}</MessageBox>) : 
+      ( <div className="row center">
+        {products.map((product)=>(
           <Product key={product._id} product={product} ></Product>
         ))}
       </div>
+      )} 
     </div>  
-  )
+  );
 }
