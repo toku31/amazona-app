0ec3294babfcc92b451b38b0fe024bb3b79c7171
garden627 <garden627@yahoo.co.jp>
2021-01-24 11:03:24 +0900

    Shopping Cart & MongoDB & User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,43 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose'
+// import data from './data.js';
+import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+
+dotenv.config(); // jwt secret
 
 const app = express();
+app.use(express.json()); // postman
+app.use(express.urlencoded({ extended: true}));  //postman
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if(product){
-      res.send(product);
-    } else {
-      res.status(400).send({ message: 'Product not Found '});
-    }
-})
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products)
-})
+// app.get('/api/products/:id', (req, res) => {
+//   const product = data.products.find((x) => x._id === req.params.id);
+//   if(product){
+//       res.send(product);
+//     } else {
+//       res.status(400).send({ message: 'Product not Found '});
+//     }
+// })
 
+// app.get('/api/products', (req, res) => {
+//   res.send(data.products)
+// })
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+app.use((err, req, res, next)=> {
+  res.status(500).send({ message: err.message });
+})
 const port = process.env.PORT  || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,34 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReduer } from './reducers/cartReducers';
 // import data from './data';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers'
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 
-const initialState = {};
+const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
+
+  cart:{
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+};
+
 // const reducer = (state, action) => {
 //   return {products: data.products}
 // }
 
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReduer,
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
